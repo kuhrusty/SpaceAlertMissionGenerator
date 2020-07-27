4cab1fe93b93726bf9226f51b803fb896c465056
Rusty Ballinger <rusty.ballinger@gmail.com>
2020-07-27 02:45:04 -0700

    Clean up warnings - remove unused singleton stuff

--- app/src/main/java/com/boarbeard/SpaceAlertApplication.java
+++ app/src/main/java/com/boarbeard/SpaceAlertApplication.java
@@ -5,16 +5,10 @@ import android.app.Application;
 import com.boarbeard.io.ExternalMedia;
 
 public class SpaceAlertApplication extends Application {
-	private static SpaceAlertApplication singleton;
-
-	public static SpaceAlertApplication getInstance() {
-		return singleton;
-	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		singleton = this;
 
 		ExternalMedia.init(this);
 	}
